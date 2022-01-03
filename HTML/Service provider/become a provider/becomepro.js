7989ad56be2a5fd564b3bf9ee3cd7ca1db5b5fb2
Mihir-bhuva <72963225+Mihir-bhuva@users.noreply.github.com>
2022-01-03 10:34:39 +0530

    Delete HTML/Service provider/become a provider directory

--- HTML/Service provider/become a provider/becomepro.js
+++
@@ -1,5 +0,0 @@
-
-jQuery('.dropdown-menu li a').click(function(){
-  var _this_img = jQuery(this).attr('data-img');
-  jQuery(this).closest('.btn-group').find(' .dropdown-toggle img').attr('src',_this_img);
-})
