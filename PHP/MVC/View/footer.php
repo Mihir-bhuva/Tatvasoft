937116c02405fdef0a94a7fcba33a7067b8eefa4
Mihir-bhuva <72963225+Mihir-bhuva@users.noreply.github.com>
2022-03-03 23:12:58 +0530

    Update footer.php

--- PHP/MVC/View/footer.php
+++ PHP/MVC/View/footer.php
@@ -50,9 +50,9 @@
 
   </div>
 </footer>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+<!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
     integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
-    crossorigin="anonymous"></script>
+    crossorigin="anonymous"></script> -->
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
