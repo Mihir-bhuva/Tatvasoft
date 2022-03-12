570e9df11029efcc5fc10b317c1956b239cd9357
Mihir-bhuva <72963225+Mihir-bhuva@users.noreply.github.com>
2022-03-12 16:24:47 +0530

    Update book-service.php

--- PHP/MVC/View/book-service.php
+++ PHP/MVC/View/book-service.php
@@ -14,7 +14,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-    <link href="https://cdn.jsdelivr.net//boxicons@2.0.5/css/boxicons.min.css" rel="stylesheet" />
+    <!-- <link href="https://cdn.jsdelivr.net//boxicons@2.0.5/css/boxicons.min.css" rel="stylesheet" /> -->
     <link rel="stylesheet" href="assets/CSS/navbar.css">
     <link rel="stylesheet" href="assets/CSS/book-service.css">
     <link rel="stylesheet" href="assets/CSS/footer.css">
