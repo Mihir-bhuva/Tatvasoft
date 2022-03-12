36a5616a48f4b076ab2433e977533fa142610dc3
Mihir-bhuva <72963225+Mihir-bhuva@users.noreply.github.com>
2022-03-12 16:25:32 +0530

    Update popup.php

--- PHP/MVC/View/popup.php
+++ PHP/MVC/View/popup.php
@@ -4,7 +4,8 @@
             "Invalid");
     </script>
 
-<?php } unset($_SESSION['loginerror']);?>
+<?php }
+unset($_SESSION['loginerror']); ?>
 
 <div class="popup" id="popup">
     <div class="popupcontent" id="popupcontent">
@@ -12,36 +13,54 @@
         <div class="popup-body">
             <form action="index.php?function=HomepageLogin" class="loginform" method="post">
                 <div>
-                    <input type="text" name="email" placeholder="Email">
-                    <input type="password" name="password" placeholder="Password">
+                    <input type="text" class="emailvalue" name="email" required placeholder="Email">
+                    <input type="password" class="passwordvalue" required name="password" placeholder="Password">
                 </div>
                 <input type="checkbox">
                 <span>
                     Remember Me
                 </span>
         </div>
-        <input type="submit" class="popup-footer continue-btn" value="Login">
+        <input type="submit" class="popup-footer logincheck continue-btn" disabled value="Login">
         </form>
         <div class="forget-password ">Forget Password</div>
         <div class="create-account">Don't Have Account? <a href="index.php?function=Createaccount" style="color: #0d6efd;">Create Account</a></div>
     </div>
 </div>
+<script>
+document.querySelector(".emailvalue").addEventListener("input",function(){
+    if (document.querySelector(".emailvalue").value != '' && document.querySelector(".passwordvalue").value != '') {
+        document.querySelector(".logincheck").removeAttribute("disabled")
+    }
+    else{
+        document.querySelector(".logincheck").setAttribute("disabled","true")
+    }
+})
+document.querySelector(".passwordvalue").addEventListener("input",function(){
+    if (document.querySelector(".emailvalue").value != '' && document.querySelector(".passwordvalue").value != '') {
+        document.querySelector(".logincheck").removeAttribute("disabled")
+    }
+    else{
+        document.querySelector(".logincheck").setAttribute("disabled","true")
+    }
+})
+</script>
 <div class="popup1" id="popup1">
     <div class="popupcontent1" id="popupcontent1">
-        <div class="popup-head1">Forget Password<span class="close1">&times;</span></div>
+        <div class="popup-head1 ">Forget Password<span class="close1">&times;</span></div>
         <div><?php
-                unset($_SESSION['sendmail']);
-                if (isset($_SESSION['sendmail'])) {
-                    $user = $_SESSION['sendmail'];
+                if (isset($_SESSION['sendmailsp'])) {
+                    $user = $_SESSION['sendmailsp'];
                     echo '<script> alert("' . $user . '")</script>';
                     // echo $user;
                 }
+                unset($_SESSION['sendmailsp']);
                 ?>
         </div>
         <div class="popup-body1">
             <form action="index.php?function=Forgetpassword" method="post">
                 <div>
-                    <input type="text" class="forgetemail" name="email" placeholder="Email">
+                    <input type="text" class="forgetemail form-control" name="email" placeholder="Email">
                 </div>
         </div>
         <input type="submit" class="popup-footer continue-btn" value="Send">
