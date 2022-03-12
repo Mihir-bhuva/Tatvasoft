e9337ca4ba1d334278d2419cef57d8865c7a7733
Mihir-bhuva <72963225+Mihir-bhuva@users.noreply.github.com>
2022-03-12 16:22:54 +0530

    Update formvalidation.js

--- PHP/MVC/assets/JS/formvalidation.js
+++ PHP/MVC/assets/JS/formvalidation.js
@@ -6,19 +6,19 @@ document.querySelector(".firstname").addEventListener("input", function () {
     var format = /^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/;
     if(Split.length==0){
         document.querySelector(".error").innerHTML="Please Enter FirstName";
-        document.querySelector(".submitbutton").style.display = "none";
+        document.querySelector(".submitbutton").setAttribute("disabled","true")
     }
     else{
         document.querySelector(".error").innerHTML = "";
-        document.querySelector(".submitbutton").style.display = "block";
+        document.querySelector(".submitbutton").removeAttribute("disabled");
     }
     if(Split[0]==" "||Split[0].match(format)!=null||(!/[^a-zA-Z]/.test(document.querySelector('.firstname').value))==false){
       document.querySelector(".error").innerHTML="White Spaces,Special Characters And Numbers Are Not Allowed in FirstName";
-      document.querySelector(".submitbutton").style.display = "none";
+      document.querySelector(".submitbutton").setAttribute("disabled","true")
     }
     else{
         document.querySelector(".error").innerHTML = "";
-        document.querySelector(".submitbutton").style.display = "block";
+        document.querySelector(".submitbutton").removeAttribute("disabled");
     }
 });
 // lastname
@@ -30,15 +30,15 @@ document.querySelector(".lastname").addEventListener("input", function () {
     }
     else{
         document.querySelector(".error").innerHTML = "";
-        document.querySelector(".submitbutton").style.display = "block";
+        document.querySelector(".submitbutton").removeAttribute("disabled");
     }
     if(Split[0]==" "||Split[0].match(format)!=null||(!/[^a-zA-Z]/.test(document.querySelector('.lastname').value))==false){
       document.querySelector(".error").innerHTML="White Spaces And Special Characters Not Allowed in Last Name";
-      document.querySelector(".submitbutton").style.display = "none";
+      document.querySelector(".submitbutton").setAttribute("disabled","true")
     }
     else{
         document.querySelector(".error").innerHTML = "";
-        document.querySelector(".submitbutton").style.display = "block";
+        document.querySelector(".submitbutton").removeAttribute("disabled");
     }
 });
 // phone
@@ -46,11 +46,11 @@ document.querySelector(".phone").addEventListener("input", function () {
    var phone = document.querySelector(".phone").value.search(/^(?:(?:\+|0{0,2})91(\s*|[\-])?|[0]?)?([6789]\d{2}([ -]?)\d{3}([ -]?)\d{4})$/)
     if(phone!=0){
       document.querySelector(".error").innerHTML="Enter Valid Phone Number";
-      document.querySelector(".submitbutton").style.display = "none";
+      document.querySelector(".submitbutton").setAttribute("disabled","true")
     }
     else{
         document.querySelector(".error").innerHTML = "";
-        document.querySelector(".submitbutton").style.display = "block";
+        document.querySelector(".submitbutton").removeAttribute("disabled");
     }
 });
 // password
@@ -58,15 +58,15 @@ document.querySelector(".password").addEventListener("input", function () {
     var password = document.querySelector(".password").value;
     if (password == "") {
         document.querySelector(".error").innerHTML = "Please Enter Password";
-        document.querySelector(".submitbutton").style.display = "none"
+        document.querySelector(".submitbutton").setAttribute("disabled","true")
     }
     else if ((password.length < 8)) {
         document.querySelector(".error").innerHTML = "Password Length must be Greater Or Equal to 8";
-        document.querySelector(".submitbutton").style.display = "none"
+        document.querySelector(".submitbutton").setAttribute("disabled","true")
     }
     else {
         document.querySelector(".error").innerHTML = "";
-        document.querySelector(".submitbutton").style.display = "block";
+        document.querySelector(".submitbutton").removeAttribute("disabled");
 
     }
 });
@@ -74,14 +74,14 @@ document.querySelector(".conformpassword").addEventListener("input", function ()
     var conformpassword = document.querySelector(".conformpassword").value;
     if (conformpassword == "") {
         document.querySelector(".error").innerHTML = "Please Enter Confirm Password";
-        document.querySelector(".submitbutton").style.display = "none"
+        document.querySelector(".submitbutton").setAttribute("disabled","true")
     }
     else if ((conformpassword.length < 8)) {
         document.querySelector(".error").innerHTML = "ConfirmPassword Length must be Greater Or Equal to 8";
-        document.querySelector(".submitbutton").style.display = "none"
+        document.querySelector(".submitbutton").setAttribute("disabled","true")
     }
     else {
         document.querySelector(".error").innerHTML = "";
-        document.querySelector(".submitbutton").style.display = "block";
+        document.querySelector(".submitbutton").removeAttribute("disabled");
     }
 });
