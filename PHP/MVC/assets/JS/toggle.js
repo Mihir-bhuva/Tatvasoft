4b3b1dd4b5df95bf05f99d6a352e34a70ef515f5
Mihir-bhuva <72963225+Mihir-bhuva@users.noreply.github.com>
2022-03-12 16:23:03 +0530

    Update toggle.js

--- PHP/MVC/assets/JS/toggle.js
+++ PHP/MVC/assets/JS/toggle.js
@@ -3,5 +3,4 @@ function classToggle() {
     navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
 }
 
-document.querySelector('.Navbar__Link-toggle')
-    .addEventListener('click', classToggle);
+document.querySelector('.Navbar__Link-toggle').addEventListener('click', classToggle);
