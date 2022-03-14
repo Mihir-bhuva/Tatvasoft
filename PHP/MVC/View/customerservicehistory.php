672855afb1ff1c49b0e5e45ca86f8a8c9f73d094
Mihir-bhuva <72963225+Mihir-bhuva@users.noreply.github.com>
2022-03-14 11:07:39 +0530

    Update customerservicehistory.php

--- PHP/MVC/View/customerservicehistory.php
+++ PHP/MVC/View/customerservicehistory.php
@@ -267,9 +267,6 @@
     <script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap4.min.js"></script>
     <script src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script>
     <script src="https://cdn.datatables.net/responsive/2.2.9/js/responsive.bootstrap5.min.js"></script>
-    <!-- <script src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script> -->
-    <!-- <script src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script>  -->
-    <!-- JavaScript Bundle with Popper -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/rateYo/2.3.2/jquery.rateyo.min.js"></script>
 </body>
 
